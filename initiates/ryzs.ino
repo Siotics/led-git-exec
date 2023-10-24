e73c19b77c5d7c344fc1ff6df22a0a85578f9341
MrRyzs <mufidnt06@gmail.com>
2023-10-24 13:36:47 +0700

    Revisi ke 5

--- initiates/ryzs.ino
+++ initiates/ryzs.ino
@@ -1,14 +1,25 @@
-void setup() {
-    Serial.begin(9600);
+//LED TRAFFIC LIGHT
+const short unsigned merah = 4, kuning = 3, hijau = 2;
+
+void setup()
+{
+  pinMode(merah, OUTPUT);  
+  pinMode(kuning, OUTPUT);
+  pinMode(hijau, OUTPUT);
 }
 
-void loop(){
-    chocolate(1);
-}
-
-void chocolate(int x){
-    Serial.print("Tampilkan = ");
-    Serial.println(x);
-    delay(500);
-    x = x + 1;
+void loop()
+{
+  digitalWrite(merah, 1);
+  digitalWrite(kuning, 0);
+  delay(2500);
+  digitalWrite(kuning, 1);
+  digitalWrite(merah, 0);
+  delay(2500);
+  digitalWrite(hijau, 1);
+  digitalWrite(kuning, 0);
+  delay(2500);
+  digitalWrite(kuning, 1);
+  digitalWrite(hijau, 0);
+  delay(2500);
 }
